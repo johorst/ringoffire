6f9f5359c5c54c577f2af6ea2efb01f410bd0c12
johorst <mart1601@web.de>
2015-12-05 10:11:37 +0100

    core zum Austausch mit Stammi usw.

---
+++ src/hello_world/core.clj
@@ -0,0 +1,52 @@
+;; When executed, this file will run a basic web server
+;; on http://localhost:8080.
+
+(ns ring.example.params
+  (:use ring.middleware.params
+        ring.util.response
+        ring.adapter.jetty))
+
+(defn page [name]
+  (str "<html><body>"
+       (if name
+         (str "Nice to meet you, " name "!")
+         (str "<form>"
+              "Name: <input name='name' type='text'>"
+              "<input type='submit'>"
+              "</form>"))
+       "</body></html>"))
+
+(defn handler [{{name "name"} :params}]
+  (-> (response (page name))
+      (content-type "text/html")))
+
+;(def sqlquery
+;    (j/query mysql-db ["select * from konten"]))
+
+(def app
+  (-> (handler wrap-params)
+;     (sqlquery)
+      ))
+
+(require '[clojure.java.jdbc :as j])
+ 
+(let [db-host "localhost"
+      db-port 3306
+      db-name "mgm_schema"]
+ 
+  (def mysql-db {:classname "com.mysql.jdbc.Driver" ; must be in classpath
+           :subprotocol "mysql"
+           :subname (str "//" db-host ":" db-port "/" db-name)
+           ; Any additional keys are passed to the driver
+           ; as driver-specific properties.
+           :user "johorst"
+           :password "666jojojo"}))
+;geht:
+;(j/insert! mysql-db :konten
+;           {:Kontonummer 9 :Kontostand 3}
+;           {:Kontonummer 10 :Kontostand 1})
+
+
+(run-jetty app {:port 8080})
+;(def server (run-jetty app {:port 8080 :join? false}))
+
