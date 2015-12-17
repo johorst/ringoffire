8506f214f851f29af90df6a026fde3b8abca552a
johorst <mart1601@web.de>
2015-12-18 00:31:49 +0100

    session mit stamm

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,37 +1,16 @@
 ;; When executed, this file will run a basic web server
 ;; on http://localhost:8080.
+(ns hello-world.core
+  (:require [ring.middleware.params :as p]
+        [ring.util.response :as r]
+        [ring.adapter.jetty :as j]
+            [clojure.java.jdbc :as jdbc])
+  (:gen-class))
 
-(ns ring.example.params
-  (:use ring.middleware.params
-        ring.util.response
-        ring.adapter.jetty))
-
-(defn page [name]
-  (str "<html><body>"
-       (if name
-         (str "Nice to meet you, " name "!")
-         (str "<form>"
-              "Name: <input name='name' type='text'>"
-              "<input type='submit'>"
-              "</form>"))
-       "</body></html>"))
-
-(defn handler [{{name "name"} :params}]
-  (-> (response (page name))
-      (content-type "text/html")))
-
-;(def sqlquery
-;    (j/query mysql-db ["select * from konten"]))
-
-(def app
-  (-> handler wrap-params))
-
-(require '[clojure.java.jdbc :as j])
- 
 (let [db-host "localhost"
       db-port 3306
       db-name "mgm_schema"]
- 
+
   (def mysql-db {:classname "com.mysql.jdbc.Driver" ; must be in classpath
            :subprotocol "mysql"
            :subname (str "//" db-host ":" db-port "/" db-name)
@@ -39,12 +18,40 @@
            ; as driver-specific properties.
            :user ""
            :password ""}))
-;geht:
-;(j/insert! mysql-db :konten
+
+
+(defn sqlquery [name]
+  
+(first (jdbc/query mysql-db [(str "select * from konten where Name='" name "'")])))
+
+(defn page [name]
+  (str "<html><body>"
+       (if name
+  (str "Nice to meet you, " name "! \n" (sqlquery name) )
+         (str "<form>"
+              "Name: <input name='name' type='text'>"
+              "<input type='submit'>"
+              "</form>"))
+       "</body></html>"))
+
+(defn handler [{{name "name"} :params}]
+  (-> (r/response (page name))
+      (r/content-type "text/html")))
+
+;(def sqlquery
+;    (j/query mysql-db ["select * from konten"]))
+
+(def app
+  (-> handler p/wrap-params))
+
+ 
 ;           {:Kontonummer 11 :Kontostand 31}
 ;           {:Kontonummer 12 :Kontostand 11})
 
 
-(run-jetty app {:port 8080})
+(defn -main
+ "Kommentar."
+ [& args]
+ (j/run-jetty app {:port 8080}))
 ;(def server (run-jetty app {:port 8080 :join? false}))
 
