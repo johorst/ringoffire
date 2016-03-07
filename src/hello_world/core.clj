05f8d9c5e6a8b82d711bcb053e04b997d1c04981
johorst <johorst@mailinator.com>
2016-03-08 00:07:33 +0100

    mit leerem sql

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -27,11 +27,10 @@
 ;(jdbc/execute! mysql-db ["UPDATE konten SET Kontostand = Kontostand + 1 WHERE NurZahlen = 0 AND Name = ?" empfaenger])) 
 ;(jdbc/update! mysql-db :konten {:Kontostand 99} ["Name = ?" name]))
 ; maybe this sets the guthaben to 99
-(jdbc/execute! mysql-db ["insert into transaktionen_geberkonten2nehmerkonten (betrag) select MIN(geberkonten.stand) from geberkonten where geberkonten.nr in ('?', '00000000-0000-0000-0000-00000025')" name])
- 
-(jdbc/execute! mysql-db ["update transaktionen_geberkonten2nehmerkonten set sender='?', empfaenger='?'  where transaktions_id = last_insert_id()" name empfaenger])
-
-(jdbc/execute! mysql-db ["update geberkonten set stand = stand - 25 where geberkonten.nr = '?' AND stand > 0" name]))
+;(jdbc/execute! mysql-db ["insert into transaktionen_geberkonten2nehmerkonten (betrag) select MIN(geberkonten.stand) from geberkonten where geberkonten.nr in ('?', '00000000-0000-0000-0000-00000025')" name])
+;(jdbc/execute! mysql-db ["update transaktionen_geberkonten2nehmerkonten set sender='?', empfaenger='?'  where transaktions_id = last_insert_id()" name empfaenger])
+;(jdbc/execute! mysql-db ["update geberkonten set stand = stand - 25 where geberkonten.nr = '?' AND stand > 0" name])
+)
 
 
 
