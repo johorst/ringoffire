4793aaffb62568302a9dafc9f2998765080f5f55
johorst <johorst@mailinator.com>
2016-03-10 20:47:51 +0100

    toggle ist besser als valid

--- db/create_tabels.sql
+++ db/create_tabels.sql
@@ -4,14 +4,13 @@ CREATE TABLE `mgm_schema`.`transaktionen_geberkonten2nehmerkonten` (
   `zeitpunkt` DATETIME NOT NULL DEFAULT NOW(),
   `betrag` INT NULL,
   `transaktions_id` INT NOT NULL AUTO_INCREMENT,
-  `valid` BINARY(1) NULL DEFAULT 1,
+  `toggle` BIT(1) NULL DEFAULT 1,
   UNIQUE INDEX `transaktions_id_UNIQUE` (`transaktions_id` ASC),
   PRIMARY KEY (`transaktions_id`));
 
-  
 CREATE TABLE `mgm_schema`.`geberkonten` (
   `nr` TEXT(32) NOT NULL,
   `stand` INT NULL);
-
+  
 insert into geberkonten values ('00000000-0000-0000-0000-00000025', 25);
 insert into geberkonten values ('12345678-1234-1234-1234-12345678', 2500);
